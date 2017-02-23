d6ea5ba12d1e88871327d55de98f9414071c08de
Nawfel Kinani <nkinani@users.noreply.github.com>
2017-02-23 10:34:26 +0100

    commiting fresh content

--- deploy.sh
+++ deploy.sh
@@ -5,30 +5,21 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 ~/Bureau/hugo_0.18.1_linux_amd64/hugo_0.18.1_linux_amd64 # if using a theme, replace by `hugo -t <yourtheme>`
 
-# clean source
-rm -rf ../labsim.github.io/*
-
-# install new site
-cp -R public/* ../labsim.github.io
-
 # Go To Public folder
-cd ../labsim.github.io/
+cd public/
 
 # Add changes to git.
 git add -A
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="rebuilding site `date` & publishing"
 git commit -m "$msg"
 
 # Push website source
 git push origin master
 
 # Come Back
-cd -
-
-# clean home
-rm -rf public
+cd ..
 
 # Push content source
 git add -A
